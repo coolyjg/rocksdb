1af01f586ad68b203ab98c3099c4d37a84cd6e5a
yanjuguang <coolyanjg@163.com>
2022-08-17 13:37:04 +0800

    add integration test

---
+++ test_integration/test1.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <string>
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+ 
+using namespace std;
+using namespace rocksdb;
+ 
+const std::string PATH = "./data_path";
+int main(int argc, char** argv){
+    DB* db;
+    Options options;
+    rocksdb::Env* spdk_env = rocksdb::NewSpdkEnv(rocksdb::Env::Default(), argv[1], argv[2], argv[3], 4096);
+    printf("\n");
+    if (spdk_env==NULL){
+        printf("spdk_env initialization fail\n");
+    }
+    options.env = spdk_env;
+    options.create_if_missing = true;
+    Status status = DB::Open(options, PATH, &db);
+    assert(status.ok());
+    Slice key("foo");
+    Slice value("bar");
+    std::string get_value;
+    status = db->Get(ReadOptions(), key, &get_value);
+    if(status.ok()){
+        printf("get %s success!!\n", get_value.c_str());
+    }else{
+        printf("get failed\n");
+    }
+    delete db;
+    return 0;
+}
